e9ed6924b0d61e29465f45793e3e4552ae683fd3
jeryd <jerydleuck@gmail.com>
2026-06-26 20:59:26 -1000

    release: bump version to 3.0.1

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
         # 1. Build the WASM frontend
         frontend = rustPlatform.buildRustPackage {
           pname = "trace-frontend";
-          version = "2.1.1";
+          version = "3.0.1";
           src = ./.;
 
           cargoLock = {
@@ -57,7 +57,7 @@
         # 2. Build the Axum backend
         backend = rustPlatform.buildRustPackage {
           pname = "trace-backend";
-          version = "2.1.1";
+          version = "3.0.1";
           src = ./.;
 
           cargoLock = {
